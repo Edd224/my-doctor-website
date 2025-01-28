61c54a7f33b96840d049c27bc863bd16fa1f2d11
Edd224 <enno225@gmail.com>
2025-01-28 21:21:39 +0100

    fix icon

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import { EnvelopeIcon, PhoneIcon, MapPinIcon } from "@heroicons/react/24/outline";
+import { PhoneIcon, MapPinIcon } from "@heroicons/react/24/outline";
 
 const TopBar: React.FC = () => {
     return (
