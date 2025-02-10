d4774b57e3ed16558a109e7366d1926fedd8b659
Edd224 <enno225@gmail.com>
2025-02-10 22:47:03 +0100

    Add article

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,23 @@
 'use client';
 
 import { Warning } from '@phosphor-icons/react';
-import { useEffect, useState } from 'react';
 import Image from "next/image"
 
 const Home: React.FC = () => {
-  const [vacationInfo, setVacationInfo] = useState<string>('');
-  const [patientTime, setPatientTime] = useState<string>('');
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchDoctorInfo = async () => {
-      try {
-        const response = await fetch('/api/doctor-info');
-        if (!response.ok) {
-          throw new Error(`Chyba: ${response.status}`);
-        }
-
-        const data = await response.json();
-        if (data.error) {
-          setError(data.error);
-        } else {
-          setVacationInfo(data.vacationInfo);
-          setPatientTime(data.patientTime);
-        }
-      } catch (err) {
-        setError('Nastala chyba pri načítavaní údajov.');
-        console.error(err);
-      }
-    };
-
-    fetchDoctorInfo();
-  }, []);
-
-  console.log('Dovolenka:', vacationInfo);
-  console.log('Pacienti objednaní na čas:', patientTime);
-
   return (
     <section
       className="relative min-h-[60vh] flex flex-col items-center justify-center rounded-20"
     >
       {/* Zobrazenie údajov o ordinácii v ľavom hornom rohu */}
       <div className="absolute top-0 left-0 border-t p-2 bg-black/20 text-white text-sm sm:text-xl rounded-10 shadow-xl">
-        {error ? (
-          <p className="text-red-500">Chyba: {error}</p>
-        ) : (
+       
           <>
-            {/* <p className="mb-2 bg-gradient-to-r from-primary p-2 rounded-10 font-semibold">Pacienti objednaní na čas: {patientTime || 'Načítava sa...'}</p> */}
             <div className="flex justify-center items-center space-x-2">
               <Warning size={32} className='animate-pulse' />
-              <p className="bg-gradient-to-r from-red-700 p-2 rounded-10 font-semibold">{vacationInfo || 'Načítava sa...'}</p>
+              <p className="bg-gradient-to-r from-red-700 p-2 rounded-10 font-semibold">Neordinujeme od 26.2. do 5.3.2025</p>
             </div>
           </>
-        )}
+      
       </div>
 
       <div className=" flex flex-col sm:flex-row justify-center items-center p-8 mt-6 border-t border-l bg-gradient-to-b to-main rounded-20 shadow-xl space-y-8 sm:space-y-0">
