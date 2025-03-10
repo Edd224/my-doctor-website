107848b96210f7b48207086b6b34bb109d457192
Edd224 <enno225@gmail.com>
2025-03-10 11:42:16 +0100

    neordinuje 13.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import { Warning } from '@phosphor-icons/react';
+import { Warning } from '@phosphor-icons/react';
 import Image from "next/image"
 
 const Home: React.FC = () => {
@@ -9,16 +9,16 @@ const Home: React.FC = () => {
       className="relative min-h-[60vh] flex flex-col items-center justify-center rounded-20"
     >
       {/* Zobrazenie údajov o ordinácii v ľavom hornom rohu */}
-      {/* <div className="absolute top-0 left-0 border-t p-2 bg-black/20 text-white text-sm sm:text-xl rounded-10 shadow-xl">
+      <div className="absolute top-0 left-0 border-t p-2 bg-black/20 text-white text-sm sm:text-xl rounded-10 shadow-xl">
        
           <>
             <div className="flex justify-center items-center space-x-2">
               <Warning size={32} className='animate-pulse' />
-              <p className="bg-gradient-to-r from-red-700 p-2 rounded-10 font-semibold">Neordinujeme od 26.2. do 5.3.2025</p>
+              <p className="bg-gradient-to-r from-red-700 p-2 rounded-10 font-semibold">Neordinujeme od 13.3. do 14.3.2025</p>
             </div>
           </>
       
-      </div> */}
+      </div>
 
       <div className=" flex flex-col sm:flex-row justify-center items-center p-8 mt-6 border-t border-l bg-gradient-to-b to-main rounded-20 shadow-xl space-y-8 sm:space-y-0">
         <div className="w-full sm:w-1/2 flex  ">
