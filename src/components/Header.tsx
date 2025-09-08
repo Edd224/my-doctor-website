18dc030a7664bdd799276cb21fe213e21ca5c896
Edd224 <enno225@gmail.com>
2025-09-08 20:42:54 +0200

    add link inhaluj spravne

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,12 +88,23 @@ const Header: React.FC = () => {
                       >
                         {menuItem.submenu.map((submenuItem, index) => (
                           <li key={index}>
-                            <Link
-                              href={submenuItem.link}
-                              className="block px-4 py-2 hover:text-white hover:bg-gradient-to-r from-text to-main shadow focus:outline-none focus:ring-4 focus:ring-slate-500/50 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-slate-500/50 relative before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white/.5)_50%,transparent_75%,transparent_100%)] before:bg-[length:250%_250%,100%_100%] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:[transition:background-position_0s_ease] hover:before:bg-[position:-100%_0,0_0] hover:before:duration-[1500ms]"
-                            >
-                              {submenuItem.title}
-                            </Link>
+                            {submenuItem.external ? (
+                              <a
+                                href={submenuItem.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="block px-4 py-2 hover:text-white hover:bg-gradient-to-r from-text to-main shadow"
+                              >
+                                {submenuItem.title}
+                              </a>
+                            ) : (
+                              <Link
+                                href={submenuItem.link}
+                                className="block px-4 py-2 hover:text-white hover:bg-gradient-to-r from-text to-main shadow"
+                              >
+                                {submenuItem.title}
+                              </Link>
+                            )}
                           </li>
                         ))}
                       </motion.ul>
@@ -154,13 +165,25 @@ const Header: React.FC = () => {
                         >
                           {menuItem.submenu.map((submenuItem, index) => (
                             <li key={index}>
-                              <Link
-                                href={submenuItem.link}
-                                className="block px-4 py-2 hover:bg-gray-200"
-                                onClick={closeMobileMenu} // Zavrie menu po kliknutí
-                              >
-                                {submenuItem.title}
-                              </Link>
+                              {submenuItem.external ? (
+                                <a
+                                  href={submenuItem.link}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="block px-4 py-2 hover:bg-gray-200"
+                                  onClick={closeMobileMenu}
+                                >
+                                  {submenuItem.title}
+                                </a>
+                              ) : (
+                                <Link
+                                  href={submenuItem.link}
+                                  className="block px-4 py-2 hover:bg-gray-200"
+                                  onClick={closeMobileMenu}
+                                >
+                                  {submenuItem.title}
+                                </Link>
+                              )}
                             </li>
                           ))}
                         </motion.ul>
